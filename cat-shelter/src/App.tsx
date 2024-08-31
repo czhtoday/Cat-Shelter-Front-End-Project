cf8631e355903aeacedbb7a362d75d956a7c487a
Zhihang CHENG <zhihang@Zhihangs-MacBook-Pro.local>
2024-08-30 17:59:37 -0700

    initial project structure

---
+++ cat-shelter/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="app">
+        
+    </div>
+  )
+}
+
+export default App
