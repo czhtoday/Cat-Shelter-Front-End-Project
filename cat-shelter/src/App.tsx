372c3f220405f35034a57b9fe4d391727feb84be
Zhihang CHENG <zhihang@Zhihangs-MacBook-Pro.local>
2024-09-03 00:30:00 -0700

    built the homepage

--- cat-shelter/src/App.tsx
+++ cat-shelter/src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/scenes/navbar";
 import { useEffect, useState } from "react";
 import { SelectedPage } from "@/shared/types";
+import Home from "@/scenes/home";
 
 function App() {
 
@@ -26,7 +27,10 @@ function App() {
     <div className="app bg-gray-20">
       <Navbar 
         isTopOfPage={isTopOfPage}
-      selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   )
 }
--- cat-shelter/tailwind.config.js
+++ cat-shelter/tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         montserrat: ["Montserrat", "sans-serif"],
       },
       content: {
-        evolvetext: "url('./assets/EvolveText.png')",
+        meowtext: "url('./assets/MeowText.png')",
         abstractwaves: "url('./assets/AbstractWaves.png')",
         sparkles: "url('./assets/Sparkles.png')",
         circles: "url('./assets/Circles.png')",
